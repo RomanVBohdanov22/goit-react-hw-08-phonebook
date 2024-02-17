de3d4425f9bcb08290dece9920cf43e6b44bbdd4
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2024-02-17 13:33:08 +0200

    Something new

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -8,6 +8,10 @@ export const selectAuth = state => state.auth.user;
 import { selectLoggedIn, selectAuth } from 'redux/auth/selectors';
 import Userbar from 'components/Userbar/Userbar';
 import Authbar from 'components/Authbar/Authbar';
+/*
+function getRandomHexColor() {
+  return `#${Math.floor((0.2 + 0.5 * Math.random()) * 16777215).toString(16)}`;
+}*/
 
 export const Layout = () => {
   const isLoggedIn = useSelector(selectLoggedIn);
@@ -15,7 +19,13 @@ export const Layout = () => {
   console.log(isAuth);
   return (
     <>
-      <ul>
+      <ul
+        style={{
+          border: '1px solid #2a2a2a',
+          padding: '8px 12px',
+          backgroundColor: '#3f51b5',
+        }}
+      >
         <li>
           <Link to={routes.HOME}>Home</Link>
         </li>
--- src/components/Userbar/Userbar.jsx
+++ src/components/Userbar/Userbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectAuth } from 'redux/auth/selectors';
+//import { login } from 'redux/auth/operations';
+//import { logOut } from 'redux/auth/operations';
+//<button type="button" onClick={() => dispatch(logOut())}>
+import { logout } from 'redux/auth/operations';
 const Userbar = () => {
   const user = useSelector(selectAuth);
+  const dispatch = useDispatch();
   return (
     <div>
       {user && <p>{user.email}</p>}
-      <button type="button">Logout</button>
+      <button type="button" onClick={() => dispatch(logout())}>
+        Logout
+      </button>
     </div>
   );
 };
