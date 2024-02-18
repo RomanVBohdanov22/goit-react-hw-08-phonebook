adbedc12c9d8cfcfe453da7b9243f478321eaefe
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2024-02-18 20:31:41 +0200

    login

--- src/components/Userbar/Userbar.jsx
+++ src/components/Userbar/Userbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectAuth } from 'redux/auth/selectors';
-import { Center, Flex, Heading } from '@chakra-ui/react';
+import { Flex, Heading } from '@chakra-ui/react'; //Center,
 import { logout } from 'redux/auth/operations';
+/*Взагалі кажучи, це можна було б ще назвати UserMenu, але вже не дуже хочу чепати*/
 const Userbar = () => {
   const user = useSelector(selectAuth);
   const dispatch = useDispatch();
   return (
     <Flex height="20vh" alignItems="center" justifyContent="center">
       <Flex direction="column" background="#1c7f51" p={12} rounded={6}>
-        {user && <Heading>{user.email}</Heading>}
+        {user && <Heading mb={6}>{user.email}</Heading>}
         <br />
         <button
           type="button"
-          style={{ backgroundColor: '#7e1b8f' }}
+          style={{ backgroundColor: '#7e1b8f', borderRadius: 6 }}
           onClick={() => dispatch(logout())}
         >
           Logout
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { login } from 'redux/auth/operations';
+import { Flex, Heading } from '@chakra-ui/react';
 
 const Login = () => {
   const [value, setValue] = useState({
@@ -22,30 +23,37 @@ const Login = () => {
     );
   };
   return (
-    <div>
+    <Flex height="20vh" alignItems="center" justifyContent="center">
       {' '}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="LoginMail">Email</label>
-        <input
-          id="LoginMail"
-          type="email"
-          name="LoginMail"
-          onChange={handleChange}
-          value={value.LoginName}
-          required
-        />
-        <label htmlFor="LoginPassword">Password</label>
-        <input
-          id="LoginPassword"
-          type="password"
-          name="LoginPassword"
-          onChange={handleChange}
-          value={value.LoginName}
-          required
-        />
-        <button type="submit">Sign In</button>
-      </form>
-    </div>
+      <Flex direction="column" background="#1c7f51" p={12} rounded={6}>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="LoginMail">Email: </label>
+          <input
+            id="LoginMail"
+            type="email"
+            name="LoginMail"
+            onChange={handleChange}
+            value={value.LoginName}
+            required
+          />{' '}
+          <label htmlFor="LoginPassword">Password: </label>
+          <input
+            id="LoginPassword"
+            type="password"
+            name="LoginPassword"
+            onChange={handleChange}
+            value={value.LoginName}
+            required
+          />{' '}
+          <button
+            type="submit"
+            style={{ backgroundColor: '#7e1b8f', borderRadius: 6, padding: 4 }}
+          >
+            Sign In
+          </button>
+        </form>
+      </Flex>
+    </Flex>
   );
 };
 
