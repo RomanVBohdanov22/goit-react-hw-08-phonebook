e768de17b1c66ee2f692569bd4810b46c20e29bf
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2024-02-18 19:59:19 +0200

    Going to nest level

--- src/components/Userbar/Userbar.jsx
+++ src/components/Userbar/Userbar.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectAuth } from 'redux/auth/selectors';
-
+import { Center, Flex, Heading } from '@chakra-ui/react';
 import { logout } from 'redux/auth/operations';
 const Userbar = () => {
   const user = useSelector(selectAuth);
   const dispatch = useDispatch();
   return (
-    <div style={{ backgroundColor: '#1c7f51' }}>
-      {user && <p>{user.email}</p>}
-      <button
-        type="button"
-        style={{ backgroundColor: '#7e1b8f' }}
-        onClick={() => dispatch(logout())}
-      >
-        Logout
-      </button>
-    </div>
+    <Flex height="20vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" background="#1c7f51" p={12} rounded={6}>
+        {user && <Heading>{user.email}</Heading>}
+        <br />
+        <button
+          type="button"
+          style={{ backgroundColor: '#7e1b8f' }}
+          onClick={() => dispatch(logout())}
+        >
+          Logout
+        </button>
+      </Flex>
+    </Flex>
   );
 };
-
+//"center" //style={{ backgroundColor: '#1c7f51' }}
 export default Userbar;
